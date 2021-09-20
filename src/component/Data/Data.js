05390103cde5797b5c25b0309deca0cde73a93cf
Alon Zylberberg <alonzylber@gmail.com>
2021-09-20 14:01:35 +0300

    working creating parking object

--- src/component/Data/Data.js
+++ src/component/Data/Data.js
@@ -13,18 +13,44 @@ export function GetData () {
         fetch('https://gisn.tel-aviv.gov.il/arcgis/rest/services/IView2/MapServer/970/query?where=1%3D1&outFields=*&f=json')
         .then(Response => Response.json())
         .then(newdata =>{
-            console.log("sucsess"); 
+            //console.log("sucsess"); 
             setData(newdata.features); 
             console.log(newdata)
         })
         .catch((error) => console.log("error"))
 
-        
     }
+
+    const handleData = () => {
+        const parkinglist = [];
+        if (data!= ""){
+            console.log("_____handleDataSection______")
+            console.log(data);
+            data.map((park) =>{
+                parkinglist.push({
+                    parkingName: park.attributes.shem_chenyon,
+                    parkingLocation: {lat: park.geometry.x, lng: park.geometry.x},
+                    parkingPrice: park.attributes.taarif_yom,
+                    parkingOccupied: park.attributes.status_chenyon
+                })  
+            })
+        }
+        else {
+            console.log(" handle data errorerror")
+        }
+
+        console.log("________afterHandle________")
+        console.log(parkinglist);
+    }
+
     useEffect( () => {
         
         fetchData();
+        
     } , []);
+    useEffect(()=>{
+        handleData();
+    },[fetchData])
 
 
     
