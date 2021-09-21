69d7ec3c5cea16a9ee0e5398a947ec75deda8794
Alon Zylberberg <alonzylber@gmail.com>
2021-09-21 23:00:51 +0300

    working getting data

--- src/component/Data/Data.js
+++ src/component/Data/Data.js
@@ -2,31 +2,34 @@ import { useEffect, useState } from "react";
 import React from 'react';
 
 
-export function GetData () {
+export function GetData (props) {
 
-    const [data,setData] = useState([""]);
-    const [parking,setParking] = useState([""])
+    const [data,setData] = useState([]);
+    const [parking,setParking] = useState([])
     //fetch data from telaviv data center
     const fetchData = () => {
 
-        console.log("effect hook");
         fetch('https://gisn.tel-aviv.gov.il/arcgis/rest/services/IView2/MapServer/970/query?where=1%3D1&outFields=*&f=json')
         .then(Response => Response.json())
         .then(newdata =>{
             //console.log("sucsess"); 
             setData(newdata.features); 
+            handleData();
             console.log(newdata)
         })
         .catch((error) => console.log("error"))
 
     }
 
-    const handleData = () => {
+    const handleData = async() => {
+
         const parkinglist = [];
-        if (data!= ""){
-            console.log("_____handleDataSection______")
-            console.log(data);
+        var parkingToAdd ={};
+        console.log("data: ",data)
+
+        if (data.length > 0){// data.length < 1
             data.map((park) =>{
+                parkingToAdd =
                 parkinglist.push({
                     parkingName: park.attributes.shem_chenyon,
                     parkingLocation: {lat: park.geometry.x, lng: park.geometry.x},
@@ -34,25 +37,28 @@ export function GetData () {
                     parkingOccupied: park.attributes.status_chenyon
                 })  
             })
+            setParking(parkinglist);
         }
+        
         else {
-            console.log(" handle data errorerror")
+            console.log("data handle errorr")
         }
-
-        console.log("________afterHandle________")
-        console.log(parkinglist);
+        
     }
 
     useEffect( () => {
+        console.log("Parking data is______",parking)
+        //props.GetData(parking);
         
-        fetchData();
+    } , [parking]);
+
+    
+     useEffect( () => {
         
-    } , []);
-    useEffect(()=>{
-        handleData();
-    },[fetchData])
-
-
+         fetchData();
+         
+     } , []);
+    
     
     return (
         <div>
