77ea67f0e4ef5f7d34b9daf25d5d58f87ef414b2
Alon Zylberberg <alonzylber@gmail.com>
2021-10-05 17:39:31 +0300

    working search

--- src/component/Search bar/SearchBar.js
+++ src/component/Search bar/SearchBar.js
@@ -1,18 +1,78 @@
 import './SearchBar.css';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom';
-
+import PlacesAutocomplete, {
+  geocodeByAddress,
+  getLatLng
+} from "react-places-autocomplete";
 
 export function SearchBar() {
-    return (
-      
-      <div className = "holder">
-        <input type="text" placeholder="Location to" className = "searchBar-to"/>
+  const [adress,setAdress] = useState("");
+  const [Coordinates,setCorrdinates] = useState({
+    lat:null,
+    lng:null
+  });
+  const handleSelect = async (value) => {
+    const results = await geocodeByAddress(value)
+    const latLng = await getLatLng(results[0])
+    setAdress(value)
+    setCorrdinates({
+      lat: latLng.lat,
+      lng: latLng.lng
+    })
+    console.log(adress)
+  }
+ 
 
-      </div>
+  return (
   
-    );
-  } 
+    <div className = "holder">
+    
+    <PlacesAutocomplete 
+      value = {adress} 
+      onChange = {setAdress} 
+      onSelect = {handleSelect}
+      >
+
+      {({getInputProps, suggestions, getSuggestionItemProps, loading}) => (
+        <div>
+
+          <input {...getInputProps({placeholder: "Type adress"})} />
+
+          <div> 
+            {loading? <div>...loading </div>:null}
+
+            
+              {suggestions.map(suggestion => {
+                const style = {
+                  backgroundColor: suggestion.active ? "#8ecae6" : "#fff"
+                };
+
+                return (
+                 
+                  <div {...getSuggestionItemProps(suggestion, { style })}>
+                    {suggestion.description}
+                  </div>
+                );
+              })}
+          </div>
+        </div>
+        
+        )}
+    </PlacesAutocomplete>
+
+
+
+
+
+
+
+      {/* <input type="text" placeholder="Location to" className = "searchBar-to"/> */}
+
+    </div>
+
+  );
+} 
   
   export default SearchBar;
   
