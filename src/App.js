4ea755c44b6a4771f4bffd61dd2c8cdc53460be1
Alon Zylberberg <alonzylber@gmail.com>
2021-10-08 11:01:30 +0300

    search and map integration

--- src/App.js
+++ src/App.js
@@ -3,32 +3,33 @@ import Map from './component/Map/Map';
 import Data from './component/Data/Data';
 import NewMap from './component/Map/NewMap'
 import SearchBar from './component/Search bar/SearchBar';
+import SearchBarNew from './component/Search bar/SearchBarNew';
 import { useState } from 'react';
 
 
 
 function App() {
  const [mapGeoLocation,setmapGeoLocation] = useState({lat: 32.0853, lng: 34.78118})
-
+ const [zoomIn,setZoomIn] = useState(14)
  //handle data recived from searchbar
+
  const handleCallBack = (childData) => {
     setmapGeoLocation(childData)
   }
 
   return (
-
+    
     <div className = "holder-app">
+
       <div className = "searchBar"> 
-        <SearchBar parentCallBack={handleCallBack} />
-        
+        {/* <SearchBar parentCallBack={handleCallBack} /> */}
+        <SearchBarNew parentCallBack={handleCallBack} />
       </div>
       <div className = "map">
         {/* <Map mapPosition = {mapGeoLocation}  /> */}
-        <NewMap lat = {32.0853} lng = {34.78118}/>
+        <NewMap lat = {mapGeoLocation.lat} lng = {mapGeoLocation.lng} zoomIn = {zoomIn}/>
         </div>
-        
     </div>
-
   );
 }
 
