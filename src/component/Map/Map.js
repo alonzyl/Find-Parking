f670b5aad33b6b09024716985183ebbfa8117226
Alon Zylberberg <alonzylber@gmail.com>
2021-10-17 19:01:00 +0300

    rendering inside map direction

--- src/component/Map/Map.js
+++ src/component/Map/Map.js
@@ -1,116 +1,117 @@
-import { withScriptjs, withGoogleMap, GoogleMap, Marker ,InfoWindow} from "react-google-maps";
-import React, { useEffect, useState } from 'react';
-import Data from '../Data/Data'
-import * as ParkingData from "../Data/Data.json"
+import React, { useEffect, useState } from 'react'; 
+import { GoogleMap, LoadScript, Marker, InfoWindow,DirectionsService,DirectionsRenderer } from '@react-google-maps/api'; // google maps api 
+import * as ParkingData from "../Data/Data.json" // parking data 
 
+const containerStyle = {
+  width: '70vw',
+  height: '100vh'
+};
 
-// function CreateMap () {
-//   const [selectedParking, setSelectedParking] = useState(null);
-//   const [mapPosition, setMapPosition] = useState({lat: 32.0853, lng: 34.78118})
-//   return (
-    
-//     <GoogleMap
-//       defaultZoom={15}
-//       defaultCenter={mapPosition}
-//     >
-//       {ParkingData.Parking.map(park=>(
-//       <Marker 
-//         key={park.Parking_Id} 
-//         position={park.Coordinates}
-//         onClick={() => {
-//             setSelectedParking(park)
-//         }}
-//       />
-//       ))}
-      
-//       {selectedParking && (
-//       <InfoWindow
-//         onCloseClick={() => {
-//           setSelectedParking(null);
-//         }}
-//         position={{
-//           lat: selectedParking.Coordinates.lat,
-//           lng: selectedParking.Coordinates.lng
-//         }}
-//       >
-//         <div>
-//           <h1>{selectedParking.Parking_Name}</h1>
-//           <p> {selectedParking.Parking_Address} </p>
-//         </div>
-//       </InfoWindow>
-//     )}
-
-//         </GoogleMap>
-//     )
-// }
-
-
-
+let passThrouge = true
 
 export function Map (props) {
-
-  const [selectedParking, setSelectedParking] = useState(null);
-  const [mapPosition, setMapPosition] = useState({lat: 32.0853, lng: 34.78118})
-  const [mapZoom,setMapZoom] = useState(15)
-  const CreateMap = () => {
-    
-  return (
-    
-    <GoogleMap
-      defaultZoom={15}
-      defaultCenter={mapPosition}
-    >
-      {ParkingData.Parking.map(park=>(
-      <Marker 
-        key={park.Parking_Id} 
-        position={park.Coordinates}
-        onClick={() => {
-            setSelectedParking(park)
-        }}
-      />
-      ))}
-      
-      {selectedParking && (
-      <InfoWindow
-        onCloseClick={() => {
-          setSelectedParking(null);
-        }}
-        position={{
-          lat: selectedParking.Coordinates.lat,
-          lng: selectedParking.Coordinates.lng
-        }}
-      >
-        <div>
-          <h1>{selectedParking.Parking_Name}</h1>
-          <p> {selectedParking.Parking_Address} </p>
-        </div>
-      </InfoWindow>
-    )}
-
-        </GoogleMap>
-    )
-  }
-  const WrappedMap = withScriptjs(withGoogleMap(CreateMap));
   
-  useEffect( () => {
-    if(props.mapPosition.lat!==null){
-      setMapPosition(props.mapPosition)
-      
-    }
-   } , [props]);
-    // return (
-    //     <div style={{ width: "80vw", height: "100vh" }}>
-    //         <WrappedMap
-    //     googleMapURL={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCkmUyo0Nh8AGWrG_QSKmGVyiBuGA528cM`}
-    //     loadingElement={<div style={{ height: `100%` }} />}
-    //     containerElement={<div style={{ height: `100%` }} />}
-    //     mapElement={<div style={{ height: `100%` }} />}
-    //   />
-                
+    const [selectedParking, setSelectedParking] = useState(null);
+    const [responsee,setResponse] = useState()
 
-    //     </div>
-    // )
-    return(<div></div>)
+    
+    const destination = {
+        lat: 32.111255,
+        lng: 34.841524
+    }
+    const center = {
+        lat: 32.0853,
+        lng: 34.78118
+      }
+
+    const directionsCallback = (response) => {
+
+        if (response !== null) {
+            if (response.status === 'OK') {
+                setResponse(response)
+                passThrouge = false
+            } else {
+              console.log('response: ', response)
+            }
+        }
+    }
+    const callDiriction = () => {
+
+        if (passThrouge === true){
+            return (
+                <DirectionsService
+                    options={{ 
+                    destination: destination,
+                    origin: center,
+                    travelMode: "WALKING"
+                    }} 
+                    callback={directionsCallback}
+                />
+            )
+        }
+        else {
+            console.log("inside else")
+        }
+    }
+    //load google maps api key 
+    useEffect( () => {
+        <LoadScript
+             googleMapsApiKey="AIzaSyCkmUyo0Nh8AGWrG_QSKmGVyiBuGA528cM">
+        </LoadScript>
+    } , []);
+
+    return (
+        <div>
+            <GoogleMap
+                mapContainerStyle={containerStyle}
+                center={center}
+                zoom={15}>
+            
+            { ParkingData.Parking.map(parkign => (
+                <Marker 
+                    key= {parkign.Parking_Id}
+                    
+                    position={{                     
+                        lat: parkign.Coordinates.lat,                     
+                        lng: parkign.Coordinates.lng 
+                    }}
+                    onClick={() => {
+                        setSelectedParking(parkign);
+                     }}
+                     visible = {true}
+                 />
+            ))
+            }
+            {selectedParking && (
+                <InfoWindow
+                    onCloseClick={() => {
+                        setSelectedParking(null);
+                    }}
+                    position={{
+                        lat: selectedParking.Coordinates.lat,
+                        lng: selectedParking.Coordinates.lng
+                    }}>
+                    <div>
+                        <h2>{selectedParking.Parking_Name}</h2>
+                        <h4>{selectedParking.Parking_Address}</h4>
+                    </div>
+                </InfoWindow>
+                )}
+            {
+               callDiriction()
+            }
+             {
+            responsee != null && (
+            <DirectionsRenderer
+              options={{ 
+                directions: responsee
+              }}
+                />
+            )
+          }
+             </GoogleMap>
+    </div>
+  )
 }
 
 export default Map;
