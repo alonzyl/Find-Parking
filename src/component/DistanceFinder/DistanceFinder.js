3596b66302a16ce760c749185d48ea1ad7a297c8
Alon Zylberberg <alonzylber@gmail.com>
2021-10-18 18:59:19 +0300

    Update DistanceFinder.js

--- src/component/DistanceFinder/DistanceFinder.js
+++ src/component/DistanceFinder/DistanceFinder.js
@@ -9,21 +9,15 @@ export function DistanceFinder(props) {
     let listOfParking
     const [finalParking,setFinalParking] = useState(null)
     let datalength 
-    let templist = []
+
     const handleData = () => {
-        
         datalength = Math.ceil(ParkingData.Parking.length/24) //the length of data separted for fetch
+        //list for fetching distance
         for (var i=1; i<=datalength;i++){
-            console.log("enter")
-             listOfParking = []
-            for (var j =1;j<=24;j++){
-                if (ParkingData.Parking[j*i])
-                {
-                    templist.push(ParkingData.Parking[j*i].Coordinates.lat)
-                    listOfParking.push(ParkingData.Parking[j*i].Coordinates) // make a new list for fetching api 
-                }
+             listOfParking = ParkingData.Parking.slice(24*(i-1),i*24)
+            for (var j =0;j<listOfParking.length;j++){
+                listOfParking[j] = listOfParking[j].Coordinates
             }
-            
             // set destination and origin before fetching
             const matrixOptions = {
                 destinations: listOfParking,
@@ -33,8 +27,6 @@ export function DistanceFinder(props) {
             service.getDistanceMatrix(matrixOptions, callback) //fetch api    
         }
     }
-    
-
     const findShortestForCallBack = (response) => {
         let minValue ={adress: response.destinationAddresses[0], duration: response.rows[0].elements[0].duration } // the first response 
         
@@ -61,14 +53,14 @@ export function DistanceFinder(props) {
                     closestParkinTemp = route
                 }
             })
+        //return closestParkinTemp
         return closestParkinTemp
-        // return closestParkinTemp
+        }
     }
-}
 
     const callback = (response, status) => {
         if (status !== "OK") {
-            alert(status ,"distance calc error");
+            alert(status ,"error");
             return;
         }
         shortestParkingList.push(findShortestForCallBack(response))
@@ -76,24 +68,22 @@ export function DistanceFinder(props) {
         if (shortestParkingList.length === datalength) {
             console.log(shortestParkingList)
             console.log("the adress of the shortest parking is",shortestroute(shortestParkingList))
-            var temp =  convertToGeo(shortestroute(shortestParkingList))
-            console.log(templist)
+            convertToGeo(shortestroute(shortestParkingList))
         }      
     }
+
     // convert adress to geo location 
     const convertToGeo =  (adress) => {
         fetch('https://maps.googleapis.com/maps/api/geocode/json?address=' + adress.adress + '&key=' + "AIzaSyCkmUyo0Nh8AGWrG_QSKmGVyiBuGA528cM")
         .then((response) => response.json())
         .then((responseJson) => {
-            var temp = responseJson.results[0].geometry.location
-            setFinalParking(temp)       
+            setFinalParking(responseJson.results[0].geometry.location)       
         })
     }
     
-
     useEffect(() => {
         if (props.origin.lat !== null ) {
-                handleData()
+            handleData()
         }
     },[props.origin]);
 
